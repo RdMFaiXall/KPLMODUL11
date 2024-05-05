4aa9c04bb585f03fe229d2592fe14e5739d77a26
unknown <rdmfaisalrj@student.telkomuniversity.ac.id>
2024-05-06 06:59:04 +0700

    deklarasi awalan

---
+++ MainProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using MatematikaLibraries;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MathLogic Matematika = new MathLogic();
+        Matematika.FPB(1, 2);
+        Matematika.KPK(1, 2);
+        /*Matematika.Turunan(1, 2);
+        Matematika.Integral(1, 2);*/
+
+        Console.WriteLine("");
+    }
+}
---
+++ MatematikaLibraries/MathLogic.cs
@@ -0,0 +1,22 @@
+﻿namespace MatematikaLibraries
+{
+    public class MathLogic
+    {
+        public void FPB(int input1, int input2)
+        {
+
+        }
+
+        public void KPK(int input1, int input2) { 
+        
+        }
+
+        public void Turunan(int[] inputInt) {
+        
+        }
+
+        public void Integral(int[] inputInt) { 
+        
+        }
+    }
+}
